f47dc74442b3363ff79ab57656cc8bfd6f98d1dc
aarthinwmsu <s563072@nwmissouri.edu>
2023-11-08 16:20:19 -0600

    Updated the controller with post menthod

--- controllers/BBWORKS.js
+++ controllers/BBWORKS.js
@@ -44,3 +44,24 @@ exports.BBWORKS_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+   // Handle BBWORKS create on POST.
+exports.BBWORKS_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new BBWORKS();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"BBWORKS_type":"goat", "cost":12, "size":"large"}
+    document.PRODUCT_NAME = req.body.PRODUCT_NAME;
+    document.PRODUCT_COLLECTION = req.PRODUCT_COLLECTION;
+    document.PRODUCT_PRICE = req.body.PRODUCT_PRICE;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
