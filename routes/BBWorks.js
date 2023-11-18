6724fae5f37a65a8342cd5f26c26df3cb0d22d04
aarthinwmsu <s563072@nwmissouri.edu>
2023-11-18 13:16:26 -0600

    Added Authentication

--- routes/BBWorks.js
+++ routes/BBWorks.js
@@ -14,6 +14,12 @@ const app = express();
 app.use(cors());
 const BBWORKS_controlers= require('../controllers/BBWORKS');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 /* GET BBWORKS */
 router.get('/', BBWORKS_controlers.BBWORKS_view_all_Page );
 /* GET detail BBWORKS page */
@@ -21,7 +27,7 @@ router.get('/detail', BBWORKS_controlers.BBWORKS_view_one_Page);
 /* GET create BBWORKS page */
 router.get('/create', BBWORKS_controlers.BBWORKS_create_Page);
 /* GET create update page */
-router.get('/update', BBWORKS_controlers.BBWORKS_update_Page);
+router.get('/update',secured, BBWORKS_controlers.BBWORKS_update_Page);
 /* GET delete BBWORKS page */
 router.get('/delete',  BBWORKS_controlers.BBWORKS_delete_Page);
 
