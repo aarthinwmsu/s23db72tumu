b97e09249bd74f68898c8762affde041d6d2f374
aarthinwmsu <s563072@nwmissouri.edu>
2023-11-27 12:31:42 -0600

    Added BBWORKS

--- models/BBWORKS.js
+++ models/BBWORKS.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose")
 const BBWORKSSchema = mongoose.Schema({
-PRODUCT_NAME: String,
-PRODUCT_COLLECTION: String,
-PRODUCT_PRICE: Number
+PRODUCT_NAME: {type: String, required: true, minLength: 5},
+PRODUCT_COLLECTION: {type: String, required: true},
+PRODUCT_PRICE: {type: String, required: true, min: 5, max: 100}
 })
 module.exports = mongoose.model("BBWORKS", BBWORKSSchema)
-
--- routes/resource.js
+++ routes/resource.js
@@ -1,30 +1,39 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors');
+//const { checkSchema } = require('express-validator');
 const app = express();
 
 app.use(cors());
 // Require controller modules.
 var api_controller = require('../controllers/api');
 var BBWORKS_controller = require('../controllers/BBWORKS');
-const validation = (req, res, next) => {
-    if ((req.body.PRODUCT_PRICE).isInt({ min: 1, max: 50 })){
-    return next();
-    }
-    else{
-    console.log("Validation Failure");
-    }
-    }
+// const validation = (req, res, next) => {
+//     if ((req.body.PRODUCT_PRICE).isInt({ min: 1, max: 50 })){
+//     return next();
+//     }
+//     else{
+//     console.log("Validation Failure");
+//     }
+//     }
+// const validation = checkSchema({
+//     PRODUCT_PRICE: {
+//       isInt: {
+//         options: { min: 1, max: 100 },
+//         errorMessage: 'Enter the price between 1$ and 100$',
+//       },
+//     },
+//   });
 /// API ROUTE ///
 // GET resources base.
 router.get('/', api_controller.api);
 /// BBWORKS ROUTES ///
 // POST request for creating a BBWORKS.
-router.post('/BBWORKS',validation, BBWORKS_controller.BBWORKS_create_post);
+router.post('/BBWORKS', BBWORKS_controller.BBWORKS_create_post);
 // DELETE request to delete BBWORKS.
 router.delete('/BBWORKS/:id', BBWORKS_controller.BBWORKS_delete);
 // PUT request to update BBWORKS.
-router.put('/BBWORKS/:id',validation, BBWORKS_controller.BBWORKS_update_put);
+router.put('/BBWORKS/:id', BBWORKS_controller.BBWORKS_update_put);
 // GET request for one BBWORKS.
 router.get('/BBWORKS/:id', BBWORKS_controller.BBWORKS_detail);
 // GET request for list of all BBWORKS items.
